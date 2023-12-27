78aa506f4bc1690bd402f662f41ba14d0ca6eb28
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-12-27 16:50:25 -0300

    Using pipe operator

--- Enum.exs
+++ Enum.exs
@@ -2,4 +2,13 @@
     def first([]), do: nil
     
     def first(list), do: hd(list)
+
+    def pipe_example() do
+      require Integer
+      
+      1..10 |>
+      Enum.map(&(&1 * 5)) |>
+      Enum.filter(&Integer.is_even(&1)) |>
+      Enum.sum()
+    end
  end
