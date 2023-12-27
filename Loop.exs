848fa6c0761ca5ec99f8490099fe6e9e86c7f65d
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-12-27 15:10:29 -0300

    Updating Loop module

--- Loop.exs
+++ Loop.exs
@@ -1,14 +1,13 @@
 defmodule MyModule.Loop do
     #Private
-    defp tabuada(_, 11, list), do: list
+    defp tabuada(_, 11), do: []
 
     #Private
-    defp tabuada(produto01, produto02, list) do
-        new_list = list ++ [produto01 * produto02]
-        tabuada(produto01, produto02 + 1, new_list)
+    defp tabuada(produto01, produto02) do
+        [produto01 * produto02] ++ tabuada(produto01, produto02 + 1)
     end
       
-    def tabuada(multiplicador) do
-        tabuada(multiplicador, 1, [])
+    def tabuada(multiplicador) when multiplicador > 0 do
+        tabuada(multiplicador, 1)
     end
 end
